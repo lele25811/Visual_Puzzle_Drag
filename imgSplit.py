c146ad7a9d9d174fc9ebdc5fdab71e61e254fc2a
L1eyan <ye.1947711@studenti.uniroma1.it>
2024-02-10 14:35:17 +0100

    1

--- imgSplit.py
+++ imgSplit.py
@@ -223,9 +223,12 @@ def check_position(imgSplit, recSplit, counter=0):
         x_rec, y_rec = recObject.posOrigin
         h, w = map(int, imgObject.size)
         # Check if the image is positioned correctly on the rectangle
-        if x_rec < x_img < x_rec + w and y_rec < y_img < y_rec + h:
+        ''' if x_rec < x_img < x_rec + w and y_rec < y_img < y_rec + h:
             cv2.rectangle(img, (x_rec, y_rec), (x_rec + w, y_rec + h), (0, 255, 0), 2)  # Green rectangle for correct position
-            counter += 1    # Increment the counter if the image is positioned correctly
+            counter += 1    # Increment the counter if the image is positioned correctly '''
+        if x_rec + w//2 - 10 < x_img + w//2 < x_rec + w//2 + 10 and y_rec + h//2 - 10 < y_img + h//2 < y_rec + h//2 + 10:
+             cv2.rectangle(img, (x_rec, y_rec), (x_rec + w, y_rec + h), (0, 255, 0), 2)  # Green rectangle for correct position
+             counter += 1    # Increment the counter if the image is positioned correctly '''
         else:
             cv2.rectangle(img, (x_rec, y_rec), (x_rec + w, y_rec + h), (0, 0, 255), 2)  # Red rectangle for incorrect position
         ''' # Check if the image is positioned correctly on the rectangle
